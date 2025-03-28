5a1a5df1a0902fc433102ff7fae2a258d3ae1479
Kaneme <113023194+KanemeDev@users.noreply.github.com>
2025-03-28 14:57:53 +0100

    Update graphique

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LHEVB - HOME</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="icon" type="image/x-icon" href="img/favicon.ico">
 </head>
 <body>
@@ -15,17 +15,17 @@
     <section>
         <div class="row d-flex gx-0">
             <div class="col text-center">
-                <div class="bg-blue mx-auto rounded-2 shadow-lg p-3 fs-3 text-black" style="width: 90%;">
-                    <h2>HOMME</h2>
-                    <a href="team/M18M.php" class="d-block text-decoration-none text-white border rounded">M18M</a>
-                    <a href="team/R1M.php" class="d-block text-decoration-none text-white border rounded">R1M</a>
+                <div class="border bg-blue mx-auto rounded-2 shadow-lg p-3 fs-3 text-black" style="width: 90%;">
+                    <h2 class="text-white">HOMME</h2>
+                    <a href="team/M18M.php" class="bg-lblue d-block text-decoration-none text-white border rounded mb-2">M18M</a>
+                    <a href="team/R1M.php" class="bg-lblue d-block text-decoration-none text-white border rounded mb-2">R1M</a>
                 </div>
             </div>
             <div class="col text-center">
-                <div class="bg-blue mx-auto rounded-2 shadow-lg p-3 fs-3 text-black" style="width: 90%;">
-                    <h2>FEMME</h2>
-                    <a href="team/M18F.php" class="d-block text-decoration-none text-white border rounded">M18F</a>
-                    <a href="team/R1F.php" class="d-block text-decoration-none text-white border rounded">R1F</a>
+                <div class="border bg-blue mx-auto rounded-2 shadow-lg p-3 fs-3 text-black" style="width: 90%;">
+                    <h2 class="text-white">FEMME</h2>
+                    <a href="team/M18F.php" class="bg-lblue d-block text-decoration-none text-white border rounded mb-2">M18F</a>
+                    <a href="team/R1F.php" class="bg-lblue d-block text-decoration-none text-white border rounded mb-2">R1F</a>
                 </div>
             </div>
         </div>
